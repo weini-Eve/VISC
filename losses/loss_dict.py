a684c9516e0045736d63bcfc77a9917a2d8e4a82
weini_Eve <914730523@qq.com>
2023-05-23 14:54:08 +0800

    first commit

---
+++ losses/loss_dict.py
@@ -0,0 +1,35 @@
+## dict for loss items 
+## map the method to its loss components
+
+
+loss_dict = {
+            'cmflow':
+                {
+                'selfLoss': [],
+                'chamferLoss': [],
+                'veloLoss':[],
+                'smoothnessLoss':[],
+                'opticalLoss': [],
+                'maskLoss': [],
+                'egoLoss': [],
+                'totalLoss': [],
+                },
+            'cmflow_t':
+                {
+                'Loss': [],
+                'chamferLoss': [],
+                'veloLoss':[],
+                'smoothnessLoss':[],
+                'egoLoss':[],
+                'maskLoss': [],
+                'superviseLoss': [],
+                'opticalLoss': [],
+                },
+            'raflow':
+                {
+                'selfLoss': [],
+                'chamferLoss': [],
+                'veloLoss':[],
+                'smoothnessLoss':[],
+                },
+            }
